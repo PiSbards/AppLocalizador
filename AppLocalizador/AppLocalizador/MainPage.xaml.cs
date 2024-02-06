bc9eecec53600de1a6bca3ab17a7379a59ea8b83
Aluno <Aluno@SNCTM202LABF02>
2024-02-05 22:18:39 -0300

    Adicionar arquivos de projeto.

---
+++ AppLocalizador/AppLocalizador/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppLocalizador
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                var endereco = ;
+                var location = await Geolocation.GetLocationAsync(endereco);
+                var locationinfo = new Location(location.Latitude, location.Longitude);
+                var options = new MapLaunchOptions { Name = "Meu local" };
+                await Map.OpenAsync(locationinfo, options);
+            }
+            catch (FeatureNotEnabledException fnsEx)
+            {
+                await DisplayAlert("Falhou", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Falhou", pEx.Message, "OK");
+            }
+            catch (Exception er)
+            {
+                await DisplayAlert("Falhou", er.Message, "OK");
+            }
+        }
+    }
+}
