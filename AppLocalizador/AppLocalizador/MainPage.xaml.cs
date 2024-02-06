1a426943fb2f3c9487990c5b4d25243ad58c245c
PiSbards <psbardellati@hotmail.com>
2024-02-06 20:18:45 -0300

    Corrigido e finalizado

--- AppLocalizador/AppLocalizador/MainPage.xaml.cs
+++ AppLocalizador/AppLocalizador/MainPage.xaml.cs
@@ -19,28 +19,22 @@ namespace AppLocalizador
         {
             base.OnAppearing();
         }
+
         
         async void Button_Clicked(object sender, EventArgs e)
         {
             
             try
             {
-                var location = await Geolocation.GetLocationAsync(new GeolocationRequest()
-                { DesiredAccuracy = GeolocationAccuracy.Best });
-                var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
-
-                var placemark = placemarks?.FirstOrDefault();
-                if (placemark != null)
+                var placemark = new Placemark()
                 {
-                    var geocodeAddress =
-                            $"AdminArea:{txtUF.Text}\n" +
-                            $"CountryName:{txtPais.Text}\n" +
-                            $"Locality:{txtCidade.Text}\n" +
-                            $"SubLocality:{txtLocal.Text}\n";
-
-                    await Map.OpenAsync(location.Latitude, location.Longitude, new MapLaunchOptions { Name = geocodeAddress });
-
-                }
+                    CountryName = txtPais.Text,
+                    AdminArea = txtUF.Text,
+                    Thoroughfare=txtLocal.Text,
+                    Locality = txtCidade.Text,
+                };
+                await Map.OpenAsync(placemark);
+                
             }
             catch (FeatureNotEnabledException fnsEx)
             {
